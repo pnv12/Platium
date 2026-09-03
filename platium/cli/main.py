ca74602546d357f8e0cca7357200884e4fed7f06
pnv12 <viktorshershun1956@gmail.com>
2026-09-03 11:17:03 +0300

    Add new commands to the CLI

--- platium/cli/main.py
+++ platium/cli/main.py
@@ -2,7 +2,16 @@
 import argparse
 import sys
 from platium.ui.display import display_banner
-from platium.cli import username_cmd, email_cmd, phone_cmd, ip_cmd, exif_cmd
+from platium.cli import (
+    username_cmd,
+    email_cmd,
+    phone_cmd,
+    ip_cmd,
+    exif_cmd,
+    social_cmd,
+    threat_cmd,
+    graph_cmd
+)
 
 def main():
     display_banner()
@@ -12,22 +21,21 @@ def main():
         epilog="Use 'platium <command> --help' for more info"
     )
     parser.add_argument("--version", action="version", version="Platium 0.1.0")
-    
+
     subparsers = parser.add_subparsers(dest="command", required=True, help="Available commands")
-    
+
     # Реєстрація всіх команд
     username_cmd.register(subparsers)
     email_cmd.register(subparsers)
     phone_cmd.register(subparsers)
     ip_cmd.register(subparsers)
     exif_cmd.register(subparsers)
-    # social_cmd.register(subparsers)
-    # threat_cmd.register(subparsers)
-    # graph_cmd.register(subparsers)
-    # report_cmd.register(subparsers)
-    
+    social_cmd.register(subparsers)
+    threat_cmd.register(subparsers)
+    graph_cmd.register(subparsers)
+
     args = parser.parse_args()
-    
+
     if hasattr(args, 'func'):
         args.func(args)
     else:
